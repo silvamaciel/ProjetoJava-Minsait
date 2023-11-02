a969f6f12098313531c722f943ae7444d5b07d66
silvamaciel <macieljunio.si@gmail.com>
2023-11-02 02:48:45 -0300

    documentação ajustada

--- src/main/java/com/minsaitjpa/desafio/Config/SwaggerConfig.java
+++ src/main/java/com/minsaitjpa/desafio/Config/SwaggerConfig.java
@@ -9,7 +9,9 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+
 @Configuration
+
 public class SwaggerConfig {
     @Bean
     public Docket api() {
